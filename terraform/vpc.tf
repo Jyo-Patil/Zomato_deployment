1a127cf5943fe77e207413da6366f1a7400c3242
Jyo-Patil <jyopatil221@gmail.com>
2025-07-25 19:41:47 +0530

    deployment

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1" # Change if you're in a different region
-}
-
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
