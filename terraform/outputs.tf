864aedab3ab797dec389b49f64ceb42798f36aba
Jyo-Patil <jyopatil221@gmail.com>
2025-07-26 15:57:57 +0530

    argocd deploy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,11 @@
-output "ecr_url" {
-  value = aws_ecr_repository.netflix.repository_url
-}
+# output "ecr_url" {
+#   value = aws_ecr_repository.netflix.repository_url
+# }
 
 output "cluster_name" {
   value = aws_eks_cluster.eks.name
+}
+
+output "ecr_url" {
+  value = aws_ecrpublic_repository.netflix.repository_uri
 }
