fbca785b3189c1e5b33156ddabba57303b56bcd5
Jyo-Patil <jyopatil221@gmail.com>
2025-07-26 15:55:26 +0530

    argocd deploy

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,11 @@
-resource "aws_ecr_repository" "netflix" {
-  name = "netflix-clone"
+# resource "aws_ecr_repository" "netflix" {
+#   name = "netflix-clone"
+# }
+
+resource "aws_ecrpublic_repository" "netflix" {
+  repository_name = "netflix-clone"
+
+  catalog_data {
+    description = "Public ECR repo for the Netflix Clone project"
+  }
 }
